28c258c6715324e4a7ab1efe3e16bc68b7fae565
livepettersdattermelkild <livepm@ntnu.no>
2020-09-07 18:41:44 +0200

    hellow world test

--- prosjekt_2/src/App.js
+++ prosjekt_2/src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          hellow world
         </a>
       </header>
     </div>
