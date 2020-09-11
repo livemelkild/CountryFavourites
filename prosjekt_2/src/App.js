2499fb781dca90bca7b431c32f037cf071ff746f
Anne Skjærseth <annehsk@stud.ntnu.no>
2020-09-11 11:43:39 +0200

    Made header and render in page

--- prosjekt_2/src/App.js
+++ prosjekt_2/src/App.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import Header from "./components/Header";
 
 function App() {
   return (
-    <h1>Hei</h1>
+    <div>
+          <h1>Hei</h1>
+          <Header />
+    </div>
   );
-}
+};
 
 export default App;
