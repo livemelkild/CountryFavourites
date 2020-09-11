f0d85a60b1078bf568d67405566144391f086f52
livepettersdattermelkild <livepm@ntnu.no>
2020-09-11 10:39:45 +0200

    save

--- prosjekt_2/src/App.js
+++ prosjekt_2/src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          hellow world
+          hello world
         </a>
       </header>
     </div>
