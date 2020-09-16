66102de04f9a436240b2d5b2e8ff1bf5792f0e12
Elise Almestad <elise.almestad1@gmail.com>
2020-09-16 14:17:54 +0200

    hei

--- prosjekt_2/src/App.js
+++ prosjekt_2/src/App.js
@@ -1,15 +1,19 @@
 import React from 'react';
 import Header from "./components/Header";
 import Country from "./components/Country";
+import logo from "./image/logo.svg";
 
 function App() {
   return (
     <div>
           <Header />
-
           <Country
           name = "Colombia"
           description = "Teste Colombia"
+          logo = <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 841.9 595.3">
+            <circle cx="50" cy="50" r="40" stroke="black"
+              stroke-width="3" fill="red" />
+          </svg>
           />
           <Country
           name = "Norway"
