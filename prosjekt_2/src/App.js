f4cd32c7e3d99117cf45ae4b35693adc7bf606e0
livepettersdattermelkild <livepm@ntnu.no>
2020-09-21 14:05:22 +0200

    første push

--- prosjekt_2/src/App.js
+++ prosjekt_2/src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import Header from "./components/Header";
 import Web_storage from "./components/Web_storage";
 import Country_spec from "./components/Country_spec";
+import Animation from "./components/Animation";
 
 
 function App() {
   return (
     <div >
           <Header />
+          <Animation />
           <Country_spec />
           <Web_storage />
     </div>
---
+++ prosjekt_2/src/components/animation_img/KeyFrames.js
@@ -0,0 +1,11 @@
+
+
+import styled, { keyframes } from 'styled-components';
+
+export const keyFrame = keyframes;
+  0% {
+    transform: translate(0px, 0px);
+  }
+  100% {
+    transform: translate(100px, 0px);
+  }
