b540acb3614cf5012d845df8605103d9307e8778
Anne Høgdahl Skjærseth <annehsk@stud.ntnu.no>
2020-09-21 14:49:09 +0200

    rotating images

--- prosjekt_2/src/App.js
+++ prosjekt_2/src/App.js
@@ -2,14 +2,12 @@ import React from 'react';
 import Header from "./components/Header";
 import Web_storage from "./components/Web_storage";
 import Country_spec from "./components/Country_spec";
-import Animation from "./components/Animation";
 
 
 function App() {
   return (
     <div >
           <Header />
-          <Animation />
           <Country_spec />
           <Web_storage />
     </div>
