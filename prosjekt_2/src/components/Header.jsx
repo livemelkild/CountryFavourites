c587e0332270548e28932319970eceff511363d6
livepettersdattermelkild <livepm@ntnu.no>
2020-09-21 17:57:22 +0200

    sound

--- prosjekt_2/src/components/Header.jsx
+++ prosjekt_2/src/components/Header.jsx
@@ -1,8 +1,12 @@
 import React from "react"
+import sound from "./mp3/usher.mp3";
+
 
 function Header() {
     return(
-        <header> <h1> Hvilket land vil du reise til? </h1></header>
+        <header> <h1> Hvilket land vil du reise til? </h1>
+        <audio controls src = {sound} autoPlay />
+        </header>
     )
 }
 
