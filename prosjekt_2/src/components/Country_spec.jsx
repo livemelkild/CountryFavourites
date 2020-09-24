aee33f9aa7729d45ad4d9a3d23ac0a85d43eb8a8
livepettersdattermelkild <livepm@ntnu.no>
2020-09-24 09:12:54 +0200

    new countries with image and sound

--- prosjekt_2/src/components/Country_spec.jsx
+++ prosjekt_2/src/components/Country_spec.jsx
@@ -10,7 +10,10 @@ import italian_sound from "./mp3/Italian.mp3";
 import portuguese_sound from "./mp3/Portuguese.mp3";
 import norwegian_sound from "./mp3/Norwegian.mp3";
 import greek_sound from "./mp3/Greek.mp3";
-
+import german_sound from "./mp3/German.mp3";
+import french_sound from "./mp3/French.mp3";
+import spanish_sound from "./mp3/Spanish.mp3";
+import english_sound from "./mp3/Greek.mp3";
 
 function Country_spec(){
 
@@ -22,17 +25,17 @@ function Country_spec(){
 
 
 
-  let poem_list = [poem_norway, poem_italy,poem_portugal,poem_greece,poem_norway, poem_italy,poem_portugal,poem_greece]
+  let poem_list = [poem_norway, poem_italy,poem_portugal,poem_greece,poem_norway, poem_italy,poem_greece,poem_greece]
 
     let liste_1 = ["/Image/map_1.svg", "/Image/italy.svg",
-    "/Image/portugal.svg", "/Image/greece.svg", "/Image/map_1.svg", "/Image/italy.svg",
-    "/Image/portugal.svg", "/Image/greece.svg"  ]
+    "/Image/portugal.svg", "/Image/greece.svg", "/Image/england.svg", "/Image/germany.svg",
+    "/Image/france.svg", "/Image/spain.svg"  ]
     let liste_2 = ["/Image/viking.svg", "/Image/wine.svg",
-    "/Image/fotball.svg", "/Image/athen.svg","/Image/viking.svg", "/Image/wine.svg",
-    "/Image/fotball.svg", "/Image/athen.svg"]
+    "/Image/fotball.svg", "/Image/athen.svg","/Image/phone-box.svg", "/Image/waitress.svg",
+    "/Image/croissant.svg", "/Image/sangria.svg"]
 
     let names = ["Norway", "Italia", "Portugal", "Hellas", "England", "Tyskland", "Frankrike", "Spania"];
-    let sound_list = [norwegian_sound,italian_sound,portuguese_sound, greek_sound, norwegian_sound,italian_sound,portuguese_sound, greek_sound]
+    let sound_list = [norwegian_sound,italian_sound,portuguese_sound, greek_sound, greek_sound,german_sound,french_sound, spanish_sound]
 
     const [img, setImage] = useState([false, false, false, false, false, false, false, false])
     const [imageUrls, setImageUrls] = useState([liste_1[0], liste_1[1], liste_1[2], liste_1[3], liste_1[4], liste_1[5], liste_1[6], liste_1[7]]);
