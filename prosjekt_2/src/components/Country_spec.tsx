bcdb648c9adb8ac133f3a546c0079931a4cfa782
livepettersdattermelkild <livepm@ntnu.no>
2020-09-28 10:43:25 +0200

    english

--- prosjekt_2/src/components/Country_spec.tsx
+++ prosjekt_2/src/components/Country_spec.tsx
@@ -43,7 +43,7 @@ function Country_spec(){
 
 
     let names = ["Norge", "Italia", "Portugal", "Hellas", "England", "Tyskland", "Frankrike", "Spania"];
-    let sound_list = [norwegian_sound,italian_sound,portuguese_sound, greek_sound, greek_sound,german_sound,french_sound, spanish_sound]
+    let sound_list = [norwegian_sound,italian_sound,portuguese_sound, greek_sound, english_sound,german_sound,french_sound, spanish_sound]
 
     const [img, setImage] = useState([false, false, false, false, false, false, false, false])
     const [imageUrls, setImageUrls] = useState([liste_1[0], liste_1[1], liste_1[2], liste_1[3], liste_1[4], liste_1[5], liste_1[6], liste_1[7]]);
@@ -111,22 +111,15 @@ function Country_spec(){
          }else{
            themeContext.setTheme('light');
          }
-<<<<<<< HEAD
+
        }}>Bytt tema 3</button>
 
        </div>
-
-      <div className="content_box">
-
-        <div className = "total-c">
-=======
-       }}>Bytt tema 3</button> </div>
         <div className="content_box">
           <div className = "total-c">
->>>>>>> 35447fd13da4c8ff2151655cf39db0010e492caf
-          {content}
+            {content}
           <br />
-        </div>
+          </div>
         </div>
         <div className="bottom">
           <div></div>
